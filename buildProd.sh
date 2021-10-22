d226ea2cab6574ce68bd8401874ec8349e3647f3
Alexander Lachmann <alexander.lachmann@gmail.com>
2021-10-22 12:51:13 -0400

    Create buildProd.sh

---
+++ buildProd.sh
@@ -0,0 +1,10 @@
+# get rid of old stuff
+docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
+docker rm $(docker ps -q -f status=exited)
+
+docker kill archs4
+docker rm archs4
+
+docker build -f DockerfilePro -t maayanlab/archs4:cloud8 .
+
+docker push maayanlab/archs4:cloud8
