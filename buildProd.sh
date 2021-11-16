4f273de84e2c69b396def5890fb7e138bbdc894c
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-11-16 11:05:26 -0500

    Update buildProd.sh

--- buildProd.sh
+++ buildProd.sh
@@ -5,7 +5,7 @@ docker rm $(docker ps -q -f status=exited)
 docker kill archs4
 docker rm archs4
 
-docker build -f DockerfilePro -t maayanlab/archs4:cloud_apache .
+docker build -f DockerfilePro -t maayanlab/archs4:cloud_apache2 .
 
-docker push maayanlab/archs4:cloud_apache
-docker run -p 5555:80 --name archs4 -it maayanlab/archs4:cloud_apache
+docker push maayanlab/archs4:cloud_apache2
+docker run -p 5555:80 --name archs4 -it maayanlab/archs4:cloud_apache2
