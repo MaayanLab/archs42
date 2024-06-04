1fa28759a5a5cb6b2948da8f955514fb08f650dd
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2024-06-04 14:21:45 -0400

    updated to 2.4

--- buildProd.sh
+++ buildProd.sh
@@ -5,6 +5,6 @@ docker rm $(docker ps -q -f status=exited)
 docker kill archs4
 docker rm archs4
 
-docker build --platform=linux/amd64 -f DockerfilePro -t maayanlab/archs4:cloud43 .
+docker build --platform=linux/amd64 -f DockerfilePro -t maayanlab/archs4:cloud44 .
 
-docker push maayanlab/archs4:cloud43
+docker push maayanlab/archs4:cloud44
